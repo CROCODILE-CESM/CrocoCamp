200837127959ac928a5e5e52ebdbbf5a151f0618
Enrico <enrico.milanese@whoi.edu>
2025-10-07 12:27:00 -0600

    conda install does not ask confirmation to install packages

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 CONDA_ENV_NAME="crococamp-2025"
-mamba env create --name "$CONDA_ENV_NAME" -f environment.yml
+mamba env create --name "$CONDA_ENV_NAME" -f environment.yml -y
 CONDA_ENV_PATH=$(conda env list | awk -v env="$CONDA_ENV_NAME" '$1 == env { print $NF }')
 mkdir -p $CONDA_ENV_PATH/etc/conda/activate.d
 echo 'module load nco' > $CONDA_ENV_PATH/etc/conda/activate.d/load_modules.sh
