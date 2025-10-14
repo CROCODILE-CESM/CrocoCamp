3dfb1fda659421811425b400be69d008d31d17e2
Enrico <enrico.milanese@whoi.edu>
2025-10-13 21:40:57 -0600

    0.1.0 already compatible with derecho, reverting 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,10 +2,8 @@
 
 All notable changes to this project will be documented in this file.
 
-## [0.1.1] - 2025-10-13
-- Add supports for NCAR-Derecho machine
-
 ## [0.1.0] - 2025-10-10
+### Added
 - Initial release for Crocodile 2025 workshop at NSF NCAR, 2025-10-13 to 2025-10-17
 - Basic functionality: 
   - model-obs comparison with MOM6 models and any obs sequence format observation file
--- install.sh
+++ install.sh
@@ -6,6 +6,5 @@ CONDA_ENV_PATH=$(conda env list | awk -v env="$CONDA_ENV_NAME" '$1 == env { prin
 mkdir -p $CONDA_ENV_PATH/etc/conda/activate.d
 echo 'module load nco' > $CONDA_ENV_PATH/etc/conda/activate.d/load_modules.sh
 echo 'export PYTHONPATH="/glade/u/home/emilanese/work/DART-Casper/observations/obs_converters/CrocoLake:\$PYTHONPATH"' > $CONDA_ENV_PATH/etc/conda/activate.d/set_pythonpath.sh
-echo 'export PYTHONPATH="/glade/u/home/emilanese/work/DART-Derecho/observations/obs_converters/CrocoLake:\$PYTHONPATH"' >> $CONDA_ENV_PATH/etc/conda/activate.d/set_pythonpath.sh
 chmod +x $CONDA_ENV_PATH/etc/conda/activate.d/load_modules.sh
 chmod +x $CONDA_ENV_PATH/etc/conda/activate.d/set_pythonpath.sh
