eef49ddded164ad9fd3f797e1b8060e5c907174e
Enrico <enrico.milanese@whoi.edu>
2025-12-16 13:41:53 -0700

    add different units for mom6 and roms salinity

--- crococamp/workflows/workflow_model_obs.py
+++ crococamp/workflows/workflow_model_obs.py
@@ -560,6 +560,11 @@ class WorkflowModelObs(workflow.Workflow):
         sort_order = ['time', 'longitude', 'latitude', 'vertical']
         merged = merged.sort_values(by=sort_order)
 
+        # DART's pmo uses different units for MOM6 and ROMS
+        if self.ocean_model == "ROMS_rutgers":
+            condition = merged["type"].str.contains("SALINITY")
+            merged["obs"] = merged["obs"].mask(condition, merged["obs"] * 1000)
+
         # Add diagnostic columns
         merged['difference'] = merged['obs'] - merged[perf_model_col]
         merged['abs_difference'] = np.abs(merged['difference'])
